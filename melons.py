79c4b75c38ab1a63051943ccf106048a948c53ba
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-12 12:18:12 -0800

    updates base price and flat fee for intl orders

--- melons.py
+++ melons.py
@@ -15,6 +15,10 @@ class AbstractMelonOrder(object):
         """Calculate price, including tax."""
 
         base_price = 5
+
+        if self.species.lower() == 'christmas':
+            base_price *= 1.5
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -48,3 +52,12 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+        """Calculate price, including tax and fees"""
+
+        if self.qty < 10:
+            flat_fee = 3
+            return super(InternationalMelonOrder, self).get_total() + flat_fee
+
+        return super(InternationalMelonOrder, self).get_total()
